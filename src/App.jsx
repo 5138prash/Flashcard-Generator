ab381e7dd700e17d962150bf742fa3ded51d4613
Prashant Tripathi <89456446+5138prash@users.noreply.github.com>
2024-12-12 20:39:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Header from "./components/Header/Header";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MyFlashCard from "./pages/MyFlashCard";
 import FlashcardDetails from "./pages/FlashCardDetails";
 import NavLinks from "./components/Navlinks/Navlinks";
@@ -9,7 +9,7 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+   <HashRouter>
       <Header />
       <div className="w-[90vw] sm:w-[80vw] mx-auto mt-10">
         <h1 className="text-[29px] font-semibold">Create Flashcard</h1>
@@ -22,8 +22,7 @@ function App() {
         <Route path="card/:index" element={<FlashcardDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Outlet />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
