d961af9b1762a2c4a9e743ca330871cd71840fa1
Prashant Tripathi <89456446+5138prash@users.noreply.github.com>
2024-12-12 20:21:07 +0530

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom"
 // Route component for handling the default (404) page not found scenario.
 export default function NotFound() {
     return (
-        <div className="w-[90vw] sm:w-[80vw] mx-auto mt-3 flex flex-col gap-5 justify-center items-center min-h-[300px]">
-            <h1 className="text-lg">
-                "404 - Page Not Found"
-            </h1>
-            <button className="border-2 border-red-500 p-1 rounded-lg bg-white">
-                <Link to={'/'}>Create Your New FlashCard</Link>
-            </button>
+        <div className="w-[90vw] sm:w-[80vw] mx-auto mt-3 flex flex-col gap-5 justify-center items-center min-h-[300px]  rounded-xl">
+            <div className="w-full flex flex-col gap-5 items-center justify-center min-h-[600px]">
+                    <p className="text-lg">Oops! No card details found.</p>
+                    <Link to={'/'}> <div className="flex justify-center  px-4 py-2 rounded-lg bg-[#cc1313] text-[#fff] hover:bg-red-600 shadow-lg shadow-[#cc1313]/75">
+                    <button >
+                        <Link to={'/'}>Back to Homepage</Link>
+                    </button>
+                    </div></Link>
+                </div>
         </div>
     )
 }
