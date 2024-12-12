9d09e81413dbddc6217e6ac0bbed0152a0d3cb83
Prashant Tripathi <89456446+5138prash@users.noreply.github.com>
2024-12-12 20:29:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import './App.css';
-import Home from './pages/Home';
-import Header from './components/Header/Header';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
-import MyFlashCard from './pages/MyFlashCard';
-import FlashcardDetails from './pages/FlashCardDetails';
-import NavLinks from './components/Navlinks/Navlinks';
-import NotFound from '.pages/NotFound.jsx'
-
+import "./App.css";
+import Home from "./pages/Home";
+import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import MyFlashCard from "./pages/MyFlashCard";
+import FlashcardDetails from "./pages/FlashCardDetails";
+import NavLinks from "./components/Navlinks/Navlinks";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <div className="w-[90vw] sm:w-[80vw] mx-auto mt-10">
-        <h1 className='text-[29px] font-semibold'>Create Flashcard</h1>
+        <h1 className="text-[29px] font-semibold">Create Flashcard</h1>
         <NavLinks />
       </div>
 
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/MyFlashCard" element={<MyFlashCard/>} />
-        <Route path="/card/:index" element={<FlashcardDetails/>} />
-        <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/MyFlashCard" element={<MyFlashCard />} />
+        <Route path="card/:index" element={<FlashcardDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      <Outlet/>
-
+      <Outlet />
     </BrowserRouter>
   );
 }
-
 
 export default App;
